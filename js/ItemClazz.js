4dc856b4ba68ee5dcf46cf768c189fd3ad5e5a72
topiniu <topiniu@outlook.com>
2017-04-06 17:52:45 +0800

    可以加载菜单了

--- js/ItemClazz.js
+++ js/ItemClazz.js
@@ -1,18 +1,27 @@
 /**
  * Created by topiniu on 2017/4/5 0005.
  */
-function Item(name){
-    this.name = name;
-}
 
-Item.prototype.createDom = function(){
+//创建Dom节点
+function createItem(name){
     var p = document.createElement("p");
-    p.innerText = this.name;
+    p.innerText = name;
 
     var div = document.createElement("div");
-    div.addClass("item");
+    $(div).addClass("item");
+    // $(div).addClass("hideBottom");
+
+    var clzNum = parseInt(Math.random()*6,10)+1;
+    $(div).addClass("clz" + clzNum);
+
+    var left = parseInt(Math.random()*80,10)+1;
+    $(div).css("left",left+"%");
+
+    var marginTop = parseInt(Math.random()*5,10)+1;
+    $(div).css("margin-top",marginTop+"em");
 
     div.appendChild(p);
 
     return div;
 }
+
