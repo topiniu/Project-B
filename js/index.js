c086383d427b687adfe7d84af82b3f5fc97d693a
topiniu <topiniu@outlook.com>
2017-04-08 04:24:49 +0800

    现在可以通过ajax跨域获取数据，下一步编写编辑面板样式

--- js/index.js
+++ js/index.js
@@ -1,18 +1,34 @@
 /**
  * Created by topiniu on 2017/4/5 0005.
  */
-var data = [
-    "黄焖鸡","盖浇饭","砂锅面","麻辣小面",
-    "山西刀削面","御味烤鸡饭","饺子","一升米",
-    "麻辣香锅","好运来米饭","港式烧腊",
-    "铁板饭","重庆鸡公煲","老狼大盘鸡",
-    "大火锅","小火锅","旋转小火锅","包子与其他小吃",
-    "黄焖茄子","韩式烤肉饭","鱼粉面","三味泡馍","麻辣烫",
-    "一号饭堂","民族餐厅","麻辣串","超市里面零食","江西瓦罐",
-    "煲仔饭","这顿省了谢谢"
-];
+var data = [];
+function getData(){
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST","http://localhost:8080/Project_B/ajax/getData",true);
+    xhr.onreadystatechange = function(){
+        if(xhr.readyState===4 && xhr.responseText != ""){
+            data = (xhr.responseText).split(",");
+        }
+    }
 
+    xhr.send(null);
+}
+function saveData(){
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST","http://localhost:8080/Project_B/ajax/writeData",true);
+    xhr.onreadystatechange = function(){
+        if(xhr.readyState===4){
+            alert(xhr.responseText);
+        }
+    }
+    // alert(data.toString());
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8");
+    xhr.send("data="+data.toString());
+}
 $(function () {
+
+    getData();//初始化data
+
    $("#reChoseBtn").on("touchstart",function () {
        $("#reChoseBtn").addClass("touch");
    });
@@ -24,11 +40,11 @@ $(function () {
 
 
     $("#btn").on("touchstart",function () {
-        $("#reChoseBtn").addClass("btnTouch");
+        $("#choseBtn").addClass("btnTouch");
     });
 
     $("#btn").on("touchend",function () {
-        $("#reChoseBtn").removeClass("btnTouch");
+        $("#choseBtn").removeClass("btnTouch");
     });
 });
 
